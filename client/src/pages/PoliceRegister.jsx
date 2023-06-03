31e744e58215a071149b63a18a716143825c9ed8
Barnil Sarma <72012568+barnilsarma@users.noreply.github.com>
2023-06-03 13:22:14 +0530

    Update PoliceRegister.jsx

--- client/src/pages/PoliceRegister.jsx
+++ client/src/pages/PoliceRegister.jsx
@@ -10,7 +10,7 @@ function RegForm(){
     return(
         <div id="reg-cont">
             <h1>Register</h1>
-            <form method='POST' action='http://127.0.0.1:5000/Reg'>
+            <form method='POST' action='https://travel-safe.vercel.app/Reg'>
                 <div className='part-1'>
                     <div className='lbls'>
                         <label>Enter Your Police Station:</label>
